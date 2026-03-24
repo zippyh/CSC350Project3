e2f979a6ad6afaefa0ce32d155a49d88b52b51e3
cheezkid26 <mcheezic@gmail.com>
2026-03-24 13:49:20 -0400

    made c4 ai player

---
+++ c4/players/ConnectFourAIPlayer.java
@@ -0,0 +1,15 @@
+package c4.players;
+
+public class ConnectFourAIPlayer extends ConnectFourPlayer {
+
+    @Override
+    public int getMove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMove'");
+    }
+
+    public boolean isAutomated(){
+        return true;
+    }
+    
+}
