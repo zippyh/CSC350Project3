3e75de3487e20fc41177ea5dfd7a5a1c8fdc05b1
nd345-tech <ndemetrio345@gmail.com>
2026-03-28 22:12:15 -0400

    Added 3playerminimax to ttt3 ai player

--- ttt3player/players/TTT3PlayerAIPlayer.java
+++ ttt3player/players/TTT3PlayerAIPlayer.java
@@ -145,7 +145,132 @@ public class TTT3PlayerAIPlayer extends TTT3PlayerAbstractPlayer {
 	}
 	
 	public int getMove(){
-		/* REPLACE WITH YOUR CODE -- Should return a number between 1 and 16 */
-		return -1;
+    	char[][] state = model.getGrid();
+    	int bestMove = -1;
+    	int bestValue = Integer.MIN_VALUE;
+
+    	for(int action : actions(state)){
+        	char[][] newState = result(state, action);
+        	int[] values = threePlayerMiniMax(newState, 5);
+
+        	int myIndex = getPlayerIndex(symbol);
+        	if(values[myIndex] > bestValue){
+            	bestValue = values[myIndex];
+            	bestMove = action;
+        	}
+    	}
+
+    	return bestMove;
+	}
+
+	private int[] threePlayerMiniMax(char[][] state, int depth){
+    	if(terminalTest(state) || depth == 0){
+        	return evaluate(state);
+		}
+
+    	char turn = getTurn(state);
+    	int playerIndex = getPlayerIndex(turn);
+
+    	int[] bestValue = null;
+
+    	for(int action : actions(state)){
+        	char[][] newState = result(state, action);
+        	int[] childValue = threePlayerMiniMax(newState, depth - 1);
+
+        	if(bestValue == null || childValue[playerIndex] > bestValue[playerIndex]){
+            	bestValue = childValue;
+        	}
+    	}
+
+    	return bestValue;
+	}
+
+	private int getPlayerIndex(char player){
+    	if(player == 'X'){
+			return 0;
+		} 
+    	if(player == 'O'){
+			return 1;
+		} 
+
+    	return 2;
+	}
+
+	private int[] evaluate(char[][] state){
+    	int[] scores = new int[3];
+
+    	char winner = getWinner(state);
+
+    	if(winner == 'X'){ 
+			scores[0] = 1000;
+		}else if(winner == 'O'){
+			scores[1] = 1000;
+		}else if(winner == '+'){ 
+			scores[2] = 1000;
+		}else if(isDraw(state)) {
+			return scores;
+		}else{
+        	scores = heuristic(state);
+    	}
+
+    	return scores;
+	}
+
+	private int[] heuristic(char[][] state){
+    	int[] scores = new int[3];
+
+    	for(int row=0; row<4; row++){
+        	for(int col=0; col<2; col++){
+            	evaluateLine(state[row][col], state[row][col+1], state[row][col+2], scores);
+        	}
+    	}
+
+    	for(int col=0; col<4; col++){
+        	for(int row=0; row<2; row++){
+            	evaluateLine(state[row][col], state[row+1][col], state[row+2][col], scores);
+        	}
+    	}
+
+    	for(int row=0; row<2; row++){
+        	for(int col=0; col<2; col++){
+            	evaluateLine(state[row][col], state[row+1][col+1], state[row+2][col+2], scores);
+        	}
+    	}
+
+    	for(int row=2; row<4; row++){
+        	for(int col=0; col<2; col++){
+            	evaluateLine(state[row][col], state[row-1][col+1], state[row-2][col+2], scores);
+        	}
+    	}
+
+    	return scores;
+	}
+
+	private void evaluateLine(char a, char b, char c, int[] scores){
+    	int[] count = new int[3];
+
+    	char[] line = {a, b, c};
+
+    	for(char cell : line){
+        	if(cell == 'X'){
+				count[0]++;
+			}else if(cell == 'O'){
+				count[1]++;
+			}
+        	else if(cell == '+'){
+				count[2]++;
+			} 
+    	}
+
+    	for(int i=0; i<3; i++){
+        	if(count[i] > 0 && count[(i+1)%3] == 0 && count[(i+2)%3] == 0){
+            	if(count[i] == 2){
+					scores[i] += 50;
+				}
+            	else if(count[i] == 1){
+					scores[i] += 10;
+				}
+        	}
+    	}
 	}
 }
